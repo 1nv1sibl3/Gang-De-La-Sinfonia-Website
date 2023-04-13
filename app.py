3c19982f8a4fc7eb84d4810a666e4d1e334054b9
1nv1sibl3 <ompritam2705@gmail.com>
2023-04-13 20:20:13 +0530

    try7

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    if os.environ.get('FLASK_ENV') == 'development':
+        app.run()
