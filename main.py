47c63ee70b86a72aacfd9ea1fd8a18c0e14a512d
DELL <longpham22092006@gmail.com>
2023-05-05 15:53:50 +0700

    create theme

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ app.static_url_path = 'assets/'
 @app.route('/')
 def home():
     return render_template('index.html')
-
+@app.route('/achievement')
+def achievement():
+    return render_template('achievement.html')
+@app.route('/members')
+def members_page():
+    return render_template('members_page.html')
+@app.route('/write_up')
+def write_up():
+    return render_template('write_up.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , port=8000, debug=True)
